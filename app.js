ff2202de2362f0468b0e848b13172ec891ea0585
Paulo Henrique Vieira Junior <Paulo Henrique Vieira Junior>
2017-05-21 17:23:46 -0300

    Consign

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
+// Requires
 var app = require('./config/server');
-var rotaNoticias = require('./app/routes/noticias')(app);
-var rotaHome = require('./app/routes/home')(app);
-var rotaForm = require('./app/routes/formulario_inclusao_noticia')(app);
 
+// Create route
 app.listen(3000, function(req, res) {
-  console.log('');
+  console.log('Sevidor ON');
 });
--- config/server.js
+++ config/server.js
@@ -1,10 +1,15 @@
 // Requires
 var express = require('express');
+var consign = require('consign');
+
 var app = express();
 
-// Configue
+// Configue app
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
+
+// Register Routes
+consign().include('app/routes').into(app);
 
 // Exports
 module.exports = app;
